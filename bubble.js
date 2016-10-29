00281d749c93a364a1b2ce560163305915ca8af8
jhhb <jboyle@bowdoin.edu>
2016-10-29 19:47:24 -0400

    everything 'works'. Need to map data, and then we gucci

---
+++ bubble.js
@@ -0,0 +1,559 @@
+var dataset = [["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "father"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "other"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "father"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["GP",  "mother"],
+["MS",  "mother"],
+["MS",  "other"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "other"],
+["MS",  "mother"],
+["MS",  "father"],
+["MS",  "father"],
+["MS",  "mother"],
+["MS",  "father"],
+["MS",  "father"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "father"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "father"],
+["MS",  "father"],
+["MS",  "other"],
+["MS",  "father"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "other"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "father"],
+["MS",  "mother"],
+["MS",  "father"],
+["MS",  "mother"],
+["MS",  "father"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "mother"],
+["MS",  "other"],
+["MS",  "mother"],
+["MS",  "other"],
+["MS",  "mother"],
+["MS",  "father"]]
+
+//gives frequencies for GPmother, father, other, / MSmother, father, other//
+var frequencyData = [[0,0,0], [0,0,0]]
+
+function calculateFrequencies(){
+  console.log(dataset[0]);
+
+
+  for(var i=0; i < dataset.length; i++){
+    if(dataset[i][0] == "MS"){
+      switch(dataset[i][1]){
+        case "mother":
+          frequencyData[0][0]+=1;
+          break;
+        case "father":
+          frequencyData[0][1]+=1;
+          break
+        case "other":
+          frequencyData[0][2]+=1;
+          break;
+        default:
+          break;
+      }
+    }
+    else{
+      switch(dataset[i][1]){
+        case "mother":
+          frequencyData[1][0]+=1;
+          break;
+        case "father":
+          frequencyData[1][1]+=1;
+          break
+        case "other":
+          frequencyData[1][2]+=1;
+          break;
+        default:
+          break;
+    }
+
+  }
+}
+
+}
+
+calculateFrequencies();
+
+var n = 3, // number of layers
+    m = 2, // number of samples per layer
+    stack = d3.layout.stack(),
+    layers = stack(d3.range(n).map(function() { return bumpLayer(m, .1); })),
+    yGroupMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y; }); }),
+    yStackMax = 100; //d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); });
+
+var margin = {top: 40, right: 10, bottom: 20, left: 10},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .domain(d3.range(m))
+    .rangeRoundBands([0, width], .08);
+
+var y = d3.scale.linear()
+    .domain([0, 100])  //yStackMa
+    .range([height, 0]);
+
+var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5)
+
+
+
+
+var color = d3.scale.linear()
+    .domain([0, n - 1])
+    .range(["#AED6F1", "#1B4F72"]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .tickSize(0)
+    .tickPadding(6)
+    .orient("bottom");
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+var layer = svg.selectAll(".layer")
+    .data(layers)
+  .enter().append("g")
+    .attr("class", "layer")
+    .style("fill", function(d, i) { return color(i); });
+
+var rect = layer.selectAll("rect")
+    .data(function(d) { return d; })
+  .enter().append("rect")
+    .attr("x", function(d) { return x(d.x); })
+    .attr("y", height)
+    .attr("width", x.rangeBand())
+    .attr("height", 0);
+
+rect.transition()
+    .delay(function(d, i) { return i * 10; })
+    .attr("y", function(d) { return y(d.y0 + d.y); })
+    .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); });
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+//added y axis
+svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + 25 + ",0)")
+    .call(yAxis)
+
+d3.selectAll("input").on("change", change);
+
+//get rid of this timeout to prevent it from auto-switching
+
+function change() {
+ // clearTimeout(timeout);
+  if (this.value === "grouped") transitionGrouped();
+  else transitionStacked();
+}
+
+function transitionGrouped() {
+  y.domain([0, yGroupMax]);
+
+  //need axis to reflect proportions
+
+
+
+  rect.transition()
+      .duration(500)
+      .delay(function(d, i) { return i * 10; })
+      .attr("x", function(d, i, j) { return x(d.x) + x.rangeBand() / n * j; })
+      .attr("width", x.rangeBand() / n)
+    .transition()
+      .attr("y", function(d) { return y(d.y); })
+      .attr("height", function(d) { return height - y(d.y); });
+}
+
+function transitionStacked() {
+  y.domain([0, yStackMax]);
+
+  rect.transition()
+      .duration(500)
+      .delay(function(d, i) { return i * 10; })
+      .attr("y", function(d) { return y(d.y0 + d.y); })
+      .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+    .transition()
+      .attr("x", function(d) { return x(d.x); })
+      .attr("width", x.rangeBand());
+}
+
+// Inspired by Lee Byron's test data generator.
+function bumpLayer(n, o) {
+  var a = [], i;
+  //for (i = 0; i < n; ++i) a[i] = o + o * Math.random();
+  for (i = 0; i < n; ++i) a[i] = 33;
+  return a.map(function(d, i) { return {x: i, y: d}; });
+}
